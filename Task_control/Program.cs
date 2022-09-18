21336ba21a268ede11a419cd832b1717c890ee01
Your Name <e@w.com>
2022-09-18 20:33:29 +0300

    Контрольная задача без комментариев

--- Task_control/Program.cs
+++ Task_control/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Задать массив из строк, вывести массив с элементами длина которых меньше или равна 3 символам
 
 Console.WriteLine("Введите размер массива:");
-int size = Convert.ToInt32(Console.ReadLine()); //Переменная size размера массива
+int size = Convert.ToInt32(Console.ReadLine()); 
                                                
-string [] array = new string[size];// создали массив размера size
+string [] array = new string[size];
 
-for (int i = 0; i < size; i++) // заполнение массива от  i до размера size
+for (int i = 0; i < size; i++) 
 {
  Console.WriteLine("Введите элемент массива:");
-  array[i] = Console.ReadLine(); //заполяем массив с консоли   
+  array[i] = Console.ReadLine(); 
 }
-Console.WriteLine($"Array: [ {String.Join("; ", array)} ]");//Вывели массив строк
+Console.WriteLine($"Array: [ {String.Join("; ", array)} ]");
 
 int sizeresult = 0;
 for (int i = 0; i < size; i++)
@@ -19,33 +19,31 @@ for (int i = 0; i < size; i++)
     if (array[i].Length <= 3)
     {
 
-        sizeresult = 1 + sizeresult;//определили размер итогового массива
-
+        sizeresult = 1 + sizeresult;
     }
 
 }
-if (sizeresult == 0)//проверка на наличие элементов заданным условиям
+if (sizeresult == 0)
 {
-    Console.WriteLine($"Нет элементов длина которых меньше или равна 3");//вывод сообщения, 
-                                            //что нет элементов соответствующих заданному условию
+    Console.WriteLine($"Нет элементов длина которых меньше или равна 3");
 }
 else
 {
-    string[] arrayresult = new string[sizeresult];//создание массива для вывода результата
+    string[] arrayresult = new string[sizeresult];
  int j = 0;
 for (int i = 0; i < size; i++)
 {
 
-    if (array[i].Length <= 3)//проверка элементов заданного масива на заданное условие
+    if (array[i].Length <= 3)
     {
        
-            arrayresult[j] = array[i];//заполнение массива результата
+            arrayresult[j] = array[i];
 
 j++;
         }
 
     }
 
-Console.WriteLine($"Result: [ {String.Join("; ", arrayresult)} ]");//вывод массива
+Console.WriteLine($"Result: [ {String.Join("; ", arrayresult)} ]");
 
 }
